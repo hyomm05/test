ba58c9da11fe59cb64edf630b9ef338bc7710a6e
Hyomin Shin <hyomm05>
2020-09-08 02:15:28 +0900

    no message

--- app/src/main/java/com/example/testtesttest/HowtoMemoActivity.java
+++ app/src/main/java/com/example/testtesttest/HowtoMemoActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.RequiresApi;
 @RequiresApi(api = Build.VERSION_CODES.M)
 
-public class HowtoGalleryActivity extends HowtouseActivity {
+public class HowtoMemoActivity extends HowtouseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.howtogallery);  // layout xml 과 자바파일을 연결
+        setContentView(R.layout.howtomemo);  // layout xml 과 자바파일을 연결
     } // end onCreate()
 
 } // end MyTwo
--- app/src/main/java/com/example/testtesttest/HowtouseActivity.java
+++ app/src/main/java/com/example/testtesttest/HowtouseActivity.java
@@ -1,14 +1,21 @@
 package com.example.testtesttest;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.roughike.bottombar.BottomBar;
 
 public class HowtouseActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.how_to_use);
         // 화면 전환 - 인텐트 날리기 (startActivity)
@@ -16,7 +23,18 @@ public class HowtouseActivity extends MainActivity {
         //    2. AndroidManifest.xml 에 Activity 를 등록한다
         //    3. Intent 객체를 만들어서 startActivity 한다
 
-        Button b = (Button)findViewById(R.id.howgallery);
+        Button a = (Button) findViewById(R.id.howphoto);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        HowtoPhotoActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+        Button b = (Button) findViewById(R.id.howgallery);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,5 +44,38 @@ public class HowtouseActivity extends MainActivity {
                 startActivity(intent); // 다음 화면으로 넘어간다
             }
         });
+
+        Button c = (Button) findViewById(R.id.howfolder);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        HowtoFolderActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+        Button d = (Button) findViewById(R.id.howmemo);
+        d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        HowtoMemoActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
+
+        Button e = (Button) findViewById(R.id.howface);
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 화면의 제어권자
+                        HowtoFaceActivity.class); // 다음 넘어갈 클래스 지정
+                startActivity(intent); // 다음 화면으로 넘어간다
+            }
+        });
     } // end onCreate()
-} // end MainActivity
+}
